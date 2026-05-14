46194819affb6408da0eb88b67a3bf2ad06f33e6
shobhit rai <s2004rai.6@gmail.com>
2026-05-14 13:03:21 +0530

    first interviewiq

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ import paymentRouter from "./routes/payment.route.js"
 
 const app = express()
 app.use(cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin:"http://localhost:5173",
     credentials:true
 }))
 
--- server/middlewares/multer.js
+++ server/middlewares/multer.js
@@ -1,9 +1,8 @@
 import multer from "multer";
-import os from "os";
 
 const storage = multer.diskStorage({
     destination: function(req, file , cb){
-        cb(null , os.tmpdir())
+        cb(null , "public")
     },
     filename: function(req , file , cb){
         const filename = Date.now() + "-" + file.originalname;
