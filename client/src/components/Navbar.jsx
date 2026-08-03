16ded3bd871202b03df0050ac0e3735eb8ecb2a4
shobhit rai <s2004rai.6@gmail.com>
2026-08-03 17:04:58 +0530

    change model

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -42,7 +42,7 @@ function Navbar() {
                     <BsRobot size={18}/>
 
                 </div>
-                <h1 className='font-semibold hidden md:block text-lg'>InterviewIQ.AI</h1>
+                <h1 className='font-semibold hidden md:block text-lg'>InterviewIQ</h1>
             </div>
 
             <div className='flex items-center gap-6  relative'>
--- server/services/openRouter.service.js
+++ server/services/openRouter.service.js
@@ -7,7 +7,7 @@ export const askAi = async (messages) => {
         }
         const response = await axios.post("https://openrouter.ai/api/v1/chat/completions",
             {
-                model: "deepseek/deepseek-v4-flash-0731",
+                model: "openai/gpt-4o-mini",
                 messages: messages
 
             },
